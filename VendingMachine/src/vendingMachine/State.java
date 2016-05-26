66fa063b675b29395f8ba484c59383f9b79fb8f8
Paulina <chatajazzy@interia.pl>
2016-05-26 19:25:48 +0200

    fxfxvcs

--- VendingMachine/src/vendingMachine/State.java
+++ VendingMachine/src/vendingMachine/State.java
@@ -3,14 +3,7 @@ package vendingMachine;
 
 public interface State {
 
-<<<<<<< HEAD
-	
-	//ulalalassadaasasassaas
-	//kolejny koment :P
-	//chyba dzia³a
-=======
 	public void insertMoney();
 	public void pickProduct();
 	public void dispense();
->>>>>>> branch 'master' of https://github.com/Jetoxixoxorix/projektVendingMachine
 }
